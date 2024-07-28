3d60087c1310042001b924768cc2cf784abcc829
Anton Patsev <patsev.anton@gmail.com>
2024-07-28 19:50:41 +0600

    init

--- examples/main.tf
+++ examples/main.tf
@@ -1,8 +1,10 @@
+data "yandex_client_config" "client" {}
+
 module "logging_group" {
   source = "../"
 
   name             = "test-logging-group"
-  folder_id        = "your-folder-id"
+  folder_id        = data.yandex_client_config.client.folder_id
   retention_period = "3600s" # 1 hour
   description      = "Example log group description"
   labels = {
