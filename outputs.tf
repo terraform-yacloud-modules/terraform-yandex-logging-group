260c00a55475d849f5fe6b345b24581152a821cb
Anton Patsev <patsev.anton@gmail.com>
2024-07-29 14:45:10 +0600

    Изменил logging_group_id на id в output

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "logging_group_id" {
+output "id" {
   description = "ID of the created logging group"
   value       = yandex_logging_group.logging_group.id
 }
 
-output "logging_group_name" {
+output "name" {
   description = "Name of the created logging group"
   value       = yandex_logging_group.logging_group.name
 }
